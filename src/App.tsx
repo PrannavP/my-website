d720e320e8ce9f89da899623c48deb673fda2dad
PrannavP <ramsarilekhi@gmail.com>
2025-11-02 00:37:14 +0545

    Revamp portfolio and added new experience section.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Projects from "./components/Projects";
 import About from "./components/About";
 import Header from "./components/Header"
 import Experience from "./components/Experience";
-import Footer from "./components/Footer";
 
 import './styles/main.css'
 
