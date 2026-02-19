3fcee7ddd7cb8042bdef147e34faa21f1e2ee3a4
PrannavP <ramsarilekhi@gmail.com>
2026-02-19 22:06:16 +0545

    Add api call to track visitors.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 // import { Header } from "./components/Header";
 import Hero  from "./components/Hero";
 import Skills from "./components/Skills";
@@ -8,7 +10,51 @@ import Experience from "./components/Experience";
 
 import './styles/main.css'
 
+type VisitorData = {
+    platform: string,
+    userAgent: string,
+    screen: {
+        width: number,
+        height: number,
+        pixelRatio: number
+    },
+    language: string
+};
+
 const App = () => {
+    // call visitor tracking api
+    useEffect(() => {
+        const data: VisitorData = {
+            screen: {
+                width: window.screen.width,
+                height: window.screen.height,
+                pixelRatio: window.devicePixelRatio
+            },
+            language: navigator.language,
+            platform: navigator.platform,
+            userAgent: navigator.userAgent
+        };
+
+        logVisitor(data);
+    }, []) // no deps cause one time calling
+
+    const logVisitor = async (data: VisitorData) => {
+        try{
+            const response = await fetch("http://localhost:3005/visit", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ data })
+            });
+
+            console.log(response);
+        }catch(err){
+            throw new Error("Error logging visitor data.")
+        }
+    };
+
     return (
         <div className="min-h-screen bg-gray-50">
             <Header />
